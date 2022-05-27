77dab4397bf3a003b8c4b4ba3d13a7f7682ad894
Sazzad Hossain <sazzad.csebubt@gmail.com>
2022-05-27 11:56:48 +0600

    updated

--- src/test/java/LoginTestRunner.java
+++ src/test/java/LoginTestRunner.java
@@ -3,12 +3,24 @@ import org.testng.annotations.Test;
 
 public class LoginTestRunner extends Setup{
     @Test
-    public void userLogin(){
+    public void userReg(){
+        driver.get("http://automationpractice.com/");
+        PageLogin pageLogin = new PageLogin(driver);
+        int id = (int)(Math.random()*(1000-1)+1);
+        String email = "testuser"+id+"@test.com";
+        String password ="testuser123";
+        pageLogin.doReg(email,password);
+    }
+//    @Test
+    public void userLogin() throws InterruptedException {
         PageLogin pageLogin = new PageLogin(driver);
         driver.get("http://automationpractice.com/");
         String name= pageLogin.doLogin();
-        Assert.assertEquals(name,"Sazzad Hossain");
-//   Assert.assertTrue(name.contains("Sazzad Hossain"), String.valueOf(true));
+//        Assert.assertEquals(name,"Sazzad Hossain");
+        Assert.assertTrue(name.contains("Sazzad Hossain"), String.valueOf(true));
         Assert.assertTrue(pageLogin.btnLogOut.isDisplayed(), String.valueOf(true));
+
+//        Thread.sleep(5000);
+//        pageLogin.btnLogOut.click();
     }
 }
--- src/test/java/PageLogin.java
+++ src/test/java/PageLogin.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
@@ -18,12 +19,63 @@ public class PageLogin {
     WebElement btnLogOut;
     @FindBy(className="header_user_info")
     List<WebElement> lblUserName;
+    @FindBy(id = "email_create")
+    WebElement txtEmailReg;
+    @FindBy(id = "SubmitCreate")
+    WebElement btnSubmitReg;
+    @FindBy(id="id_gender1")
+    WebElement rb1;
+    @FindBy(id="customer_firstname")
+    WebElement txtFirstName;
+    @FindBy(id="customer_lastname")
+    WebElement txtLastName;
+    @FindBy(id = "days")
+    WebElement cbDate;
+    @FindBy(id="months")
+    WebElement cbMonths;
+    @FindBy(id="years")
+    WebElement cbYears;
+    @FindBy(xpath = "//span[contains(text(),'Register')]")
+    WebElement btnSubmitAccount;
+    @FindBy(name="address1")
+    WebElement txtAddress;
+    @FindBy(name="city")
+    WebElement txtCity;
+    @FindBy(name="postcode")
+    WebElement txtPostCode;
+    @FindBy(name="id_state")
+    WebElement cbState;
+    @FindBy(name="phone_mobile")
+    WebElement txtMobile;
+
     WebDriver driver;
 
     public PageLogin(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+   public void doReg(String email,String password){
+        btnLogin.click();
+        txtEmailReg.sendKeys(email);
+        btnSubmitReg.click();
+       rb1.click();
+       txtFirstName.sendKeys("Test");
+       txtLastName.sendKeys("User");
+       textPassword.sendKeys(password);
+       Select date=new Select(cbDate);
+       date.selectByValue("5");
+       Select month=new Select(cbMonths);
+       month.selectByValue("3");
+       Select year=new Select(cbYears);
+       year.selectByValue("1990");
+       txtAddress.sendKeys("Alabama");
+       txtCity.sendKeys("Alabama");
+       txtPostCode.sendKeys("10101");
+       Select option=new Select(cbState);
+       option.selectByVisibleText("Alabama");
+       txtMobile.sendKeys("000000000000");
+       btnSubmitAccount.click();
+   }
     public String doLogin(){
         btnLogin.click();
         textEmail.sendKeys("forazi@gmail.com");
